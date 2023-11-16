f41c783737bc03a09fde6f797affaea7cf066711
Faheem-saif <faheem000saif@gmail.com>
2023-11-16 22:42:38 +0500

    hooks used

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,10 @@ export default function Home() {
   setname('Muhammad Faheem')
  }
 
-
+const [value,setvalue]= useState('attractive')
+const namefunn=() =>{
+  setvalue("great")
+}
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -20,6 +23,8 @@ export default function Home() {
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro voluptatem iste ab dolorem, quo ullam eum perspiciatis cum ipsa quos animi eius, delectus neque expedita fugit ducimus soluta. Labore dolor velit quisquam eaque laborum non cum dolorum repellat adipisci deserunt! Hic rerum possimus voluptates ullam nostrum, obcaecati corrupti. Vero, nulla.</p>
      <h1>My Name is {name}</h1>
      <button onClick={namefun}>Get Full Name</button>
+     <button onClick={namefunn}>Get Full value</button>
+     <button onClick={namefunn}>Get Full value</button>
      
 
      
